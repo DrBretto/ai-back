f1ffe3dbc454b1f39b813bc1114fd1fd8bc9dff3
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-05 18:05:14 -0400

    updated login endpoint for debug

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -4,42 +4,43 @@ const AuthService = require('./auth-service');
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
-authRouter.post('/login', jsonBodyParser, (req, res, next) => {
+authRouter.post('/login', jsonBodyParser, async (req, res, next) => {
   const { email, password } = req.body;
   const loginUser = { email, password };
 
   console.log('Login endpoint hit with:', loginUser);
 
-  for (const [key, value] of Object.entries(loginUser))
-    if (value === null)
+  for (const [key, value] of Object.entries(loginUser)) {
+    if (value == null)
       return res.status(400).json({
         error: `Missing '${key}' in request body`,
       });
+  }
 
   try {
-    const dbUser = AuthService.getUserWithUserName(
+    const dbUser = await AuthService.getUserWithUserName(
       req.app.get('db'),
       loginUser.email
     );
-
     console.log('User fetched from DB:', dbUser);
 
-    if (!dbUser)
+    if (!dbUser) {
       return res.status(400).json({
         error: 'Incorrect email or password',
       });
+    }
 
-    const compareMatch = AuthService.comparePasswords(
+    const compareMatch = await AuthService.comparePasswords(
       loginUser.password,
       dbUser.password
     );
-
     console.log('Password compare result:', compareMatch);
 
-    if (!compareMatch)
+    if (!compareMatch) {
       return res.status(400).json({
         error: 'Incorrect email or password',
       });
+    }
 
     const sub = dbUser.email;
     const payload = { user_id: dbUser.id };
@@ -51,5 +52,3 @@ authRouter.post('/login', jsonBodyParser, (req, res, next) => {
     next(err);
   }
 });
-
-module.exports = authRouter;
