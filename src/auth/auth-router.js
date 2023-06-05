70070395c1af49d5c0542dd87bcdd1958be58dc3
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-05 12:46:50 -0400

    updated login endpoint for debug

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -4,41 +4,52 @@ const AuthService = require('./auth-service');
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
-authRouter.post('/login', jsonBodyParser, (req, res, next) => {
+authRouter.post('/login', jsonBodyParser, async (req, res, next) => {
   const { email, password } = req.body;
   const loginUser = { email, password };
-  console.log(req.body);
+
+  console.log('Login endpoint hit with:', loginUser);
 
   for (const [key, value] of Object.entries(loginUser))
-    if (value === null)
+    if (value == null)
       return res.status(400).json({
         error: `Missing '${key}' in request body`,
       });
 
-  AuthService.getUserWithUserName(req.app.get('db'), loginUser.email)
-    .then((dbUser) => {
-      if (!dbUser)
-        return res.status(400).json({
-          error: 'Incorrect email or password',
-        });
+  try {
+    const dbUser = await AuthService.getUserWithUserName(
+      req.app.get('db'),
+      loginUser.email
+    );
 
-      return AuthService.comparePasswords(
-        loginUser.password,
-        dbUser.password
-      ).then((compareMatch) => {
-        if (!compareMatch)
-          return res.status(400).json({
-            error: 'Incorrect email or password',
-          });
+    console.log('User fetched from DB:', dbUser);
 
-        const sub = dbUser.email;
-        const payload = { user_id: dbUser.id };
-        res.send({
-          authToken: AuthService.createJwt(sub, payload),
-        });
+    if (!dbUser)
+      return res.status(400).json({
+        error: 'Incorrect email or password',
       });
-    })
-    .catch(next);
+
+    const compareMatch = await AuthService.comparePasswords(
+      loginUser.password,
+      dbUser.password
+    );
+
+    console.log('Password compare result:', compareMatch);
+
+    if (!compareMatch)
+      return res.status(400).json({
+        error: 'Incorrect email or password',
+      });
+
+    const sub = dbUser.email;
+    const payload = { user_id: dbUser.id };
+    res.send({
+      authToken: AuthService.createJwt(sub, payload),
+    });
+  } catch (err) {
+    console.error('Error during login:', err);
+    next(err);
+  }
 });
 
 module.exports = authRouter;
