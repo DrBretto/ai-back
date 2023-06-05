14690ca45b47f1e18dc012c35b9968e235131a2b
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-04 23:27:54 -0400

    updating for more debugging fun!

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -6,6 +6,12 @@ const xss = require('xss');
 const UsersService = {
   hasUserWithUserName(db, email) {
     console.log('Checking if user exists');
+    console.log(
+      db('users')
+        .where({ email })
+        .first()
+        .then((user) => !!user)
+    );
     return db('users')
       .where({ email })
       .first()
