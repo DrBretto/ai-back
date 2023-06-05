c5054b4fa142ef0e53ecce19db1471a3768c01ee
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-04 22:26:14 -0400

    redid user service

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -4,16 +4,37 @@
 const bcrypt = require('bcryptjs');
 const xss = require('xss');
 
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL =
-  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
-
 const UsersService = {
-  hasUserWithUserName(db, user_name) {
+  hasUserWithUserName(db, email) {
     return db('users')
-      .where({ user_name })
+      .where({ email })
       .first()
       .then((user) => !!user);
   },
+
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password must be longer than 8 characters';
+    }
+    if (password.length > 72) {
+      return 'Password must be less than 72 characters';
+    }
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces';
+    }
+    if (!/[0-9]/.test(password)) {
+      return 'Password must contain one digit';
+    }
+    if (!/[A-Z]/.test(password)) {
+      return 'Password must contain one upper case letter';
+    }
+    return null;
+  },
+
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+
   insertUser(db, newUser) {
     return db
       .insert(newUser)
@@ -21,30 +42,11 @@ const UsersService = {
       .returning('*')
       .then(([user]) => user);
   },
-  validatePassword(password) {
-    if (password.length < 8) {
-      return 'Password be longer than 8 characters';
-    }
-    if (password.length > 72) {
-      return 'Password be less than 72 characters';
-    }
-    if (password.startsWith(' ') || password.endsWith(' ')) {
-      return 'Password must not start or end with empty spaces';
-    }
-    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return 'Password must contain one upper case, lower case, number and special character';
-    }
-    return null;
-  },
-  hashPassword(password) {
-    return bcrypt.hash(password, 12);
-  },
+
   serializeUser(user) {
-    
     return {
       id: user.id,
-      full_name: xss(user.full_name),
-      user_name: xss(user.user_name),
+      email: xss(user.email),
       date_created: new Date(user.date_created),
     };
   },
