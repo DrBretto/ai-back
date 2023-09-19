bc859872eabf0056a86735808ac6510e61e9010e
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-09-19 10:45:49 -0400

    adjusted stock history router for monthly data

--- src/stocks/stocks-router.js
+++ src/stocks/stocks-router.js
@@ -4,10 +4,10 @@ const StocksService = require('./stocks-service');
 const stocksRouter = express.Router();
 
 stocksRouter.get('/history', async (req, res, next) => {
-  const { stockSymbol, startDate, endDate } = req.query;
+  const { stockSymbol, month } = req.query; // Changed to month to match service
 
   try {
-    const data = await StocksService.fetchStockHistory(stockSymbol, startDate, endDate);
+    const data = await StocksService.fetchStockHistory(stockSymbol, month);
     res.json(data);
   } catch (error) {
     next(error);
