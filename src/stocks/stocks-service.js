d728c2206ad3ccb5ebc60c3adb794a6ab53bc810
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-09-19 10:39:16 -0400

    adjusted URL for historical data collection

--- src/stocks/stocks-service.js
+++ src/stocks/stocks-service.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
 const StocksService = {
-  async fetchStockHistory(stockSymbol, slice) {
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&slice=${slice}&apikey=B91GX8DBBAR32QM0`;
+  async fetchStockHistory(stockSymbol, month) {
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&${month}&outputsize=full&apikey=B91GX8DBBAR32QM0`;
     const response = await fetch(url);
     const data = await response.json();
 
