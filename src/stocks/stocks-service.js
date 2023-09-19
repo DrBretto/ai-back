2123849158d28fe12034d4e64cdadf412bd8b554
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-09-19 11:27:49 -0400

    fixed URL in API call (Again)

--- src/stocks/stocks-service.js
+++ src/stocks/stocks-service.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const StocksService = {
   async fetchStockHistory(stockSymbol, month) {
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&${month}&outputsize=full&apikey=B91GX8DBBAR32QM0`;
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&month=${month}&outputsize=full&apikey=B91GX8DBBAR32QM0`;
     const response = await fetch(url);
     const data = await response.json();
     console.log('data:', data);
