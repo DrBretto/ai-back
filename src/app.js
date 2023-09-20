3c890d26d282eab8190aec7241af5c32cf76a316
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-09-20 09:52:57 -0400

    testing scheduler

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { NODE_ENV } = require('./config');
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const stocksRouter = require('./stocks/stocks-router');
+const StocksService = require('./stocks/stocks-service');
 
 const dataRouter = require('./data/data-router.js');
 
@@ -39,5 +40,37 @@ app.use(function errorHandler(error, req, res) {
   }
   res.status(500).json(response);
 });
+
+const cron = require('node-cron');
+
+// Function to get the last 24 months
+const getLast24Months = () => {
+  const dates = [];
+  let currentDate = new Date();
+  
+  for (let i = 0; i < 24; i++) {
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    
+    dates.push(`${year}-${month}`);
+    
+    currentDate.setMonth(currentDate.getMonth() - 1);
+  }
+  
+  return dates;
+};
+
+const months = getLast24Months();
+
+cron.schedule('*/5 * * * *', async () => {
+  for (const month of months) {
+    await StocksService.fetchStockHistory('JDST', month);
+    await StocksService.fetchStockHistory('NUGT', month);
+  }
+});
+
+
+
+
 
 module.exports = app;
