19fbb5227eb86768589e0cbce1b7f5d79a6c7557
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-10 13:19:59 -0400

    more restructring for split endpoints

--- src/stocks/stocks-router.js
+++ src/stocks/stocks-router.js
@@ -10,12 +10,40 @@ stocksRouter.get('/history', async (req, res, next) => {
   const db = req.app.get('db');
 
   try {
-    const data = await StocksService.fetchStockHistory(db, stockSymbol); // Pass stockSymbol to fetchStockHistory
-    res.json(data);
+    await StocksService.fetchHistoricalData(db, stockSymbol); // Call the fetchHistoricalData function
+    res.status(200).json({ message: 'Historical data fetched successfully' });
   } catch (error) {
     next(error);
   }
 });
+
+stocksRouter.get('/refresh', async (req, res, next) => {
+  console.log('Inside /refresh route', req.query);
+
+  const { stockSymbol } = req.query; // Get stockSymbol from query params
+  const db = req.app.get('db');
+
+  try {
+    await StocksService.fetchTodaysData(db, stockSymbol); // Call the fetchTodaysData function
+    res.status(200).json({ message: "Today's data fetched successfully" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// stocksRouter.get('/history', async (req, res, next) => {
+//   console.log('Inside /history route', req.query);
+
+//   const { stockSymbol } = req.query; // Get stockSymbol from query params
+//   const db = req.app.get('db');
+
+//   try {
+//     const data = await StocksService.fetchStockHistory(db, stockSymbol); // Pass stockSymbol to fetchStockHistory
+//     res.json(data);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // stocksRouter.get('/refreshData', async (req, res, next) => {
 //   const db = req.app.get('db');
--- src/stocks/stocks-service.js
+++ src/stocks/stocks-service.js
@@ -6,109 +6,109 @@ const StocksService = {
     return stock ? stock.stock_id : null;
   },
 
-  async fetchStockHistory(db, stockSymbol) {
-    // Get the stock ID
-    const stockId = await this.getStockId(db, stockSymbol); // Assuming getStockId is a method in the same service
+  // async fetchStockHistory(db, stockSymbol) {
+  //   // Get the stock ID
+  //   const stockId = await this.getStockId(db, stockSymbol); 
+  //   console.log('Inside fetchStockHistory', stockId);
+    
+  //   // First, find the last date in the DB for this stock
+  //   const lastDateInDBRow = await db('stockhistory')
+  //     .where('stock_id', stockId)
+  //     .max('date_time');
 
-    console.log('Inside fetchStockHistory', stockId);
-    // First, find the last date in the DB for this stock
-    const lastDateInDBRow = await db('stockhistory')
-      .where('stock_id', stockId)
-      .max('date_time');
+  //   const lastDateInDB = new Date(lastDateInDBRow[0].max);
+  //   const lastDayOfMonth = new Date(
+  //     lastDateInDB.getFullYear(),
+  //     lastDateInDB.getMonth() + 1,
+  //     0
+  //   ).getDate();
+  //   const lastMinuteOfDay = 59;
 
-    const lastDateInDB = new Date(lastDateInDBRow[0].max);
-    const lastDayOfMonth = new Date(
-      lastDateInDB.getFullYear(),
-      lastDateInDB.getMonth() + 1,
-      0
-    ).getDate();
-    const lastMinuteOfDay = 59;
+  //   let url;
+  //   let monthToFetch = lastDateInDB.getMonth() + 1; // JavaScript months are 0-11
 
-    let url;
-    let monthToFetch = lastDateInDB.getMonth() + 1; // JavaScript months are 0-11
+  //   console.log(
+  //     'Inside fetchStockHistory dates',
+  //     lastDateInDBRow,
+  //     lastDayOfMonth,
+  //     monthToFetch
+  //   );
 
-    console.log(
-      'Inside fetchStockHistory dates',
-      lastDateInDBRow,
-      lastDayOfMonth,
-      monthToFetch
-    );
+  //   if (Date.now() - lastDateInDB <= 6000000) {
+  //     // 100 minutes in milliseconds
+  //     // If the last data point is within 100 minutes, pull the last 100 data points
+  //     url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&outputsize=full&apikey=B91GX8DBBAR32QM0`;
+  //   } else {
+  //     // Check if the last date is the last minute of the last day of the month
+  //     if (
+  //       lastDateInDB.getDate() === lastDayOfMonth &&
+  //       lastDateInDB.getMinutes() === lastMinuteOfDay
+  //     ) {
+  //       monthToFetch++; // Increment the month if it's the last minute of the last day
+  //       if (monthToFetch > 12) {
+  //         monthToFetch = 1; // Reset to January if it goes over December
+  //       }
+  //     }
+  //     // Otherwise, pull the data for the entire month
+  //     url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&month=${monthToFetch}&outputsize=full&apikey=B91GX8DBBAR32QM0`;
+  //   }
 
-    if (Date.now() - lastDateInDB <= 6000000) {
-      // 100 minutes in milliseconds
-      // If the last data point is within 100 minutes, pull the last 100 data points
-      url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&outputsize=compact&apikey=B91GX8DBBAR32QM0`;
-    } else {
-      // Check if the last date is the last minute of the last day of the month
-      if (
-        lastDateInDB.getDate() === lastDayOfMonth &&
-        lastDateInDB.getMinutes() === lastMinuteOfDay
-      ) {
-        monthToFetch++; // Increment the month if it's the last minute of the last day
-        if (monthToFetch > 12) {
-          monthToFetch = 1; // Reset to January if it goes over December
-        }
-      }
-      // Otherwise, pull the data for the entire month
-      url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&month=${monthToFetch}&outputsize=full&apikey=B91GX8DBBAR32QM0`;
-    }
+  //   console.log('About to fetch data from Alpha Vantage');
 
-    console.log('About to fetch data from Alpha Vantage');
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   const timeSeries = data['Time Series (1min)'];
 
-    const response = await fetch(url);
-    const data = await response.json();
-    const timeSeries = data['Time Series (1min)'];
+  //   // Convert the lastDateInDBRow to a comparable format (if needed)
+  //   const lastDateTimeComparable = new Date(
+  //     lastDateInDBRow[0].max
+  //   ).toISOString();
 
-    // Convert the lastDateInDBRow to a comparable format (if needed)
-    const lastDateTimeComparable = new Date(
-      lastDateInDBRow[0].max
-    ).toISOString();
+  //   // Filter the timeSeries object
+  //   const filteredTimeSeries = Object.fromEntries(
+  //     Object.entries(timeSeries).filter(
+  //       ([dateTime, lastDateTimeComparable]) =>
+  //         new Date(dateTime).toISOString() > lastDateTimeComparable
+  //     )
+  //   );
 
-    // Filter the timeSeries object
-    const filteredTimeSeries = Object.fromEntries(
-      Object.entries(timeSeries).filter(
-        ([dateTime, lastDateTimeComparable]) =>
-          new Date(dateTime).toISOString() > lastDateTimeComparable
-      )
-    );
+  //   console.log('lastDateTimeComparable: ', lastDateTimeComparable);
 
-    console.log('lastDateTimeComparable: ', lastDateTimeComparable);
+  //   // Loop through the time series and insert new data into the DB
+  //   for (const [dateTime, stockData] of Object.entries(filteredTimeSeries)) {
+  //     const existingRecord = await db('stockhistory')
+  //       .where({
+  //         stock_id: stockId,
+  //         date_time: dateTime,
+  //       })
+  //       .first();
 
-    // Loop through the time series and insert new data into the DB
-    for (const [dateTime, stockData] of Object.entries(filteredTimeSeries)) {
-      const existingRecord = await db('stockhistory')
-        .where({
-          stock_id: stockId,
-          date_time: dateTime,
-        })
-        .first();
+  //     if (!existingRecord) {
+  //       const closePrice = stockData['4. close'];
+  //       const highPrice = stockData['2. high'];
+  //       const lowPrice = stockData['3. low'];
+  //       const volume = stockData['5. volume'];
 
-      if (!existingRecord) {
-        const closePrice = stockData['4. close'];
-        const highPrice = stockData['2. high'];
-        const lowPrice = stockData['3. low'];
-        const volume = stockData['5. volume'];
+  //       console.log(
+  //         'Data to be inserted:',
+  //         stockId,
+  //         closePrice,
+  //         highPrice,
+  //         lowPrice,
+  //         volume
+  //       );
 
-        console.log(
-          'Data to be inserted:',
-          stockId,
-          closePrice,
-          highPrice,
-          lowPrice,
-          volume
-        );
-
-        await db('stockhistory').insert({
-          stock_id: stockId,
-          date_time: dateTime,
-          closing_price: closePrice,
-          high_price: highPrice,
-          low_price: lowPrice,
-          volume: volume,
-        });
-      }
-    }
-  },
+  //       await db('stockhistory').insert({
+  //         stock_id: stockId,
+  //         date_time: dateTime,
+  //         closing_price: closePrice,
+  //         high_price: highPrice,
+  //         low_price: lowPrice,
+  //         volume: volume,
+  //       });
+  //     }
+  //   }
+  // },
 
   async fetchHistoricalData(db, stockSymbol) {
     const stockId = await this.getStockId(db, stockSymbol);
@@ -123,7 +123,7 @@ const StocksService = {
 
   async fetchTodaysData(db, stockSymbol) {
     const stockId = await this.getStockId(db, stockSymbol);
-    const todayUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&outputsize=compact&apikey=B91GX8DBBAR32QM0`;
+    const todayUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=1min&adjusted=true&outputsize=full&apikey=B91GX8DBBAR32QM0`;
 
     await this.fetchDataAndInsert(db, stockId, todayUrl);
   },
@@ -157,19 +157,19 @@ const StocksService = {
 
 
 
-  // async fetchAndSaveStocks(db) {
-  //   const stocks = ['JDST', 'NUGT'];
-  //   for (const stock of stocks) {
-  //     const lastDataPoint = await db('stockhistory')
-  //       .where({ stock_id: stock })
-  //       .orderBy('date_time', 'desc')
-  //       .first();
-  //     const lastDateTime = lastDataPoint ? lastDataPoint.date_time : null;
+  async fetchAndSaveStocks(db) {
+    const stocks = ['JDST', 'NUGT'];
+    for (const stock of stocks) {
+      const lastDataPoint = await db('stockhistory')
+        .where({ stock_id: stock })
+        .orderBy('date_time', 'desc')
+        .first();
+      const lastDateTime = lastDataPoint ? lastDataPoint.date_time : null;
 
-  //     // Fetch and save data for this stock
-  //     await this.fetchStockHistory(db, stock, lastDateTime);
-  //   }
-  // },
+      // Fetch and save data for this stock
+      await this.fetchStockHistory(db, stock, lastDateTime);
+    }
+  },
 };
 
 module.exports = StocksService;
