5325a7845496ec372cd2afd4c4526c77e82fdfa6
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-12 10:26:20 -0400

    updated seeds, but mostly jsut refreshing deployment

--- seeds/seed.data.sql
+++ seeds/seed.data.sql
@@ -1,8 +1,1 @@
-TRUNCATE TABLE users;
-
-INSERT INTO users (id, user_name, full_name, password,  date_created)
-VALUES
-
-  (1, 'Demo', 'Demo', '$2a$12$nWn9FFfvnRYRPeV7nWej.uGITxh347TMwIicz0Y9CkOPvTexO.HM6',  '2020-09-04 19:02:53');
-
-INSERT INTO Stocks (symbol) VALUES ('JDST', 'NUGT');
+INSERT INTO Stocks (symbol) VALUES ('JDST'), ('NUGT');
