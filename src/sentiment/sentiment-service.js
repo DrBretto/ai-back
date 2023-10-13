094024725e45fe3c185f7b0f6bc7dd56aa17edcb
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-13 12:31:15 -0400

    testing cheerio

--- src/sentiment/sentiment-service.js
+++ src/sentiment/sentiment-service.js
@@ -3,67 +3,71 @@ const cheerio = require('cheerio');
 
 const SentimentService = {
   async scrapeTradingView() {
-    const url = 'https://www.tradingview.com/news/';
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-    const articles = [];
+    try {
+      const url = 'https://www.tradingview.com/news/';
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+      const articles = [];
 
-    $('a[href^="/news/"]').each((index, element) => {
-      const articleUrl = $(element).attr('href');
-      const articleTitle = $(element).text();
-      if (
-        articleTitle.toLowerCase().includes('gold') ||
-        articleTitle.toLowerCase().includes('us dollar')
-      ) {
-        articles.push({
-          url: `https://www.tradingview.com${articleUrl}`,
-          title: articleTitle,
-        });
-      }
-    });
+      $('a[href^="/news/"]').each((index, element) => {
+        const articleUrl = $(element).attr('href');
+        const articleTitle = $(element).text();
+        if (
+          articleTitle.toLowerCase().includes('gold') ||
+          articleTitle.toLowerCase().includes('us dollar')
+        ) {
+          articles.push({
+            url: `https://www.tradingview.com${articleUrl}`,
+            title: articleTitle,
+          });
+        }
+      });
 
-    return articles;
-  },
-
-  async fetchTradingViewNews() {
-    const { data } = await axios.get('https://www.tradingview.com/news/');
-    const $ = cheerio.load(data);
-    const articles = [];
-
-    $('your-article-selector').each((index, element) => {
-      const title = $(element).find('your-title-selector').text();
-      const url = $(element).find('your-url-selector').attr('href');
-      if (
-        title.toLowerCase().includes('gold') ||
-        title.toLowerCase().includes('us dollar')
-      ) {
-        articles.push({ title, url });
-      }
-    });
-
-    return articles;
+      console.log('Scraped articles:', articles);
+      return articles;
+    } catch (error) {
+      console.error('Error in scrapeTradingView:', error);
+      return [];
+    }
   },
 
   async fetchArticleContent(url) {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const articleContent = $('your-article-content-selector').text();
-    return articleContent;
+    try {
+      const { data } = await axios.get(url);
+      const $ = cheerio.load(data);
+
+      // Get date from breadcrumbs
+      const date = $('[class*="breadcrumbs"]').text(); // Replace with actual partial class name
+
+      // Get entire article content
+      const articleContent = $('body').text(); // Replace with a more specific selector if available
+
+      console.log('Fetched article content:', articleContent);
+      console.log('Fetched date:', date);
+
+      return { date, content: articleContent };
+    } catch (error) {
+      console.error('Error in fetchArticleContent:', error);
+      return '';
+    }
   },
 
   async performSentimentAnalysis() {
-    const articles = await this.fetchTradingViewNews();
-    const articleContents = [];
+    try {
+      const articles = await this.scrapeTradingView();
+      const articleContents = [];
 
-    for (const article of articles) {
-      const content = await this.fetchArticleContent(article.url);
-      articleContents.push({ title: article.title, content });
+      for (const article of articles) {
+        const content = await this.fetchArticleContent(article.url);
+        articleContents.push({ title: article.title, content });
+      }
+
+      console.log('Article contents:', articleContents);
+      return articleContents;
+    } catch (error) {
+      console.error('Error in performSentimentAnalysis:', error);
+      return [];
     }
-
-    // Here, you can run the articleContents through your sentiment analysis model
-    // ...
-
-    return articleContents; // For now, just returning the scraped content
   },
 };
 
