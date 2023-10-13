dc582f9ebb5913d801bb2edf4752ef83cafc0a2f
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-13 12:12:17 -0400

    runnings scrape one layer deeper

--- src/sentiment/sentiment-service.js
+++ src/sentiment/sentiment-service.js
@@ -24,6 +24,47 @@ const SentimentService = {
 
     return articles;
   },
+
+  async fetchTradingViewNews() {
+    const { data } = await axios.get('https://www.tradingview.com/news/');
+    const $ = cheerio.load(data);
+    const articles = [];
+
+    $('your-article-selector').each((index, element) => {
+      const title = $(element).find('your-title-selector').text();
+      const url = $(element).find('your-url-selector').attr('href');
+      if (
+        title.toLowerCase().includes('gold') ||
+        title.toLowerCase().includes('us dollar')
+      ) {
+        articles.push({ title, url });
+      }
+    });
+
+    return articles;
+  },
+
+  async fetchArticleContent(url) {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const articleContent = $('your-article-content-selector').text();
+    return articleContent;
+  },
+
+  async performSentimentAnalysis() {
+    const articles = await this.fetchTradingViewNews();
+    const articleContents = [];
+
+    for (const article of articles) {
+      const content = await this.fetchArticleContent(article.url);
+      articleContents.push({ title: article.title, content });
+    }
+
+    // Here, you can run the articleContents through your sentiment analysis model
+    // ...
+
+    return articleContents; // For now, just returning the scraped content
+  },
 };
 
 module.exports = SentimentService;
