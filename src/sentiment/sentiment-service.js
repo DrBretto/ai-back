5eb75b54785f950ff66169e3b49f46fb25bdbc7d
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-20 12:03:01 -0400

    GPT drunk again

--- src/sentiment/sentiment-service.js
+++ src/sentiment/sentiment-service.js
@@ -72,7 +72,7 @@ const SentimentService = {
           'published_at_end': endDate,
           'per_page': 100,
           'categories.taxonomy': 'iab-qag',
-          'categories.id[]': 'IAB22',
+          'categories.id[]': 'IAB13',
           'language[]': 'en',
         },
       });
