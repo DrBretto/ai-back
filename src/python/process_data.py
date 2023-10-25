ef0bdaf101e4a60719098802e63e035b0de08024
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-25 13:24:04 -0400

    testing

--- src/python/process_data.py
+++ src/python/process_data.py
@@ -6,5 +6,6 @@ def process_data(data):
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as file:
-        data = json.load(file)
+        data_str = file.read()
+        data = json.loads(data_str)  # Parse the string back into a JSON object
         process_data(data)
--- src/python/python_api.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from data_router import data_router
-
-app = Flask(__name__)
-app.register_blueprint(data_router, url_prefix='/api/data')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
