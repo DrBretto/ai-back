47ac7eba6bfa1f4da3835c21f8448da9996ddc11
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-25 12:27:29 -0400

    fixing errors

--- src/data/data-service.js
+++ src/data/data-service.js
@@ -3,31 +3,35 @@ const { exec } = require('child_process');
 const path = require('path');
 
 const DataService = {
-  async processPrices(db) {
-    const prices = await db
-      .select('closing_price')
-      .from('stockrealtime');
+  async getPricingData(db) {
+    const prices = await db.select('closing_price').from('stockrealtime');
 
-    const data = prices.map(price => price.closing_price);
+    const data = prices.map((price) => price.closing_price);
 
     // Write data to a temporary file
     const filePath = path.join(__dirname, 'tempData.json');
     fs.writeFileSync(filePath, JSON.stringify(data));
 
     return new Promise((resolve, reject) => {
-      exec(`python3 ${path.join(__dirname, '..', 'python', 'process_data.py')} ${filePath}`, (error, stdout) => {
-        // Delete the temporary file
-        fs.unlinkSync(filePath);
+      exec(
+        `python3 ${path.join(
+          __dirname,
+          '..',
+          'python',
+          'process_data.py'
+        )} ${filePath}`,
+        (error, stdout) => {
+          // Delete the temporary file
+          fs.unlinkSync(filePath);
 
-        if (error) {
-          reject(error);
-        } else {
-          resolve(stdout);
+          if (error) {
+            reject(error);
+          } else {
+            resolve(stdout);
+          }
         }
-      });
+      );
     });
   },
 };
-
 module.exports = DataService;
-
