786fad06774d8b8b44b0e49afaf03ee6dc9801ca
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-25 14:23:44 -0400

    fixing up data pipeline

--- src/python/process_data.py
+++ src/python/process_data.py
@@ -2,11 +2,8 @@ import sys
 import json
 
 def process_data(data):
-    print(f'Data read from file: {data}')
     print(len(data))
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as file:
-        data = json.load(file)  # This line reads the data as JSON, which will be a list in Python
-        process_data(data)
-
+    data = json.loads(sys.argv[1])
+    process_data(data)
