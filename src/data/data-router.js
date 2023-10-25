c52d10443bb5e26f874d861e13945e5854639efa
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-25 14:53:41 -0400

    logs

--- src/data/data-router.js
+++ src/data/data-router.js
@@ -29,13 +29,45 @@ dataRouter.get('/process-prices', async (req, res, next) => {
       scriptError += data.toString();
     });
 
-    process.on('close', (code) => {
-      fs.unlinkSync(tempFilePath); // Delete the temporary file
-      if (code !== 0) {
-        console.error('Python script error:', scriptError);
-        return next(new Error('Python script failed'));
+    dataRouter.get('/process-prices', async (req, res, next) => {
+      try {
+        const db = req.app.get('db');
+        const prices = await DataService.getPricingData(db);
+
+        // Write data to a temporary file
+        const tempFilePath = path.join(__dirname, 'tempData.json');
+        fs.writeFileSync(tempFilePath, JSON.stringify(prices));
+
+        const scriptPath = path.join(
+          __dirname,
+          '..',
+          'python',
+          'process_data.py'
+        );
+        const process = spawn('python3', [scriptPath, tempFilePath]); // Pass the file path, not the data
+
+        let scriptOutput = '';
+        let scriptError = '';
+
+        process.stdout.on('data', (data) => {
+          scriptOutput += data.toString();
+        });
+
+        process.stderr.on('data', (data) => {
+          scriptError += data.toString();
+        });
+
+        process.on('close', (code) => {
+          fs.unlinkSync(tempFilePath); // Delete the temporary file
+          if (code !== 0) {
+            console.error('Python script error:', scriptError);
+            return next(new Error('Python script failed'));
+          }
+          res.json({ count: scriptOutput });
+        });
+      } catch (error) {
+        next(error);
       }
-      res.json({ count: scriptOutput });
     });
   } catch (error) {
     next(error);
