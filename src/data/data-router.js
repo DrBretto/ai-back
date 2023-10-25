5c4b230492b6061243bd9ef3b5f77584f6013152
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-25 13:17:14 -0400

    testing

--- src/data/data-router.js
+++ src/data/data-router.js
@@ -1,33 +1,38 @@
-// data-router.js
-const express = require('express');
-const DataService = require('./data-service');
-const { spawn } = require('child_process');
+const fs = require('fs');
 const path = require('path');
+const { spawn } = require('child_process');
+const DataService = require('./data-service');
+const express = require('express');
 
 const dataRouter = express.Router();
 
 dataRouter.get('/process-prices', async (req, res, next) => {
   try {
+    const db = req.app.get('db');
+    const prices = await DataService.getPricingData(db);
 
-      const db = req.app.get('db');
-      const prices = await DataService.getPricingData(db);
-  
-      const scriptPath = path.join(__dirname, '..', 'python', 'process_data.py');
-      const process = spawn('python3', [scriptPath, JSON.stringify(prices)]);
+    // Write data to a temporary file
+    const tempFilePath = path.join(__dirname, 'tempData.json');
+    fs.writeFileSync(tempFilePath, JSON.stringify(prices));
+
+    const scriptPath = path.join(__dirname, '..', 'python', 'process_data.py');
+    const process = spawn('python3', [scriptPath, tempFilePath]); // Pass the file path, not the data
+
     let scriptOutput = '';
-    let scriptError = '';  // Add this line to capture error output
-    
+    let scriptError = '';
+
     process.stdout.on('data', (data) => {
       scriptOutput += data.toString();
     });
 
-    process.stderr.on('data', (data) => {  // Add this block to capture error output
+    process.stderr.on('data', (data) => {
       scriptError += data.toString();
     });
 
     process.on('close', (code) => {
+      fs.unlinkSync(tempFilePath); // Delete the temporary file
       if (code !== 0) {
-        console.error('Python script error:', scriptError);  // Log the error output
+        console.error('Python script error:', scriptError);
         return next(new Error('Python script failed'));
       }
       res.json({ count: scriptOutput });
@@ -36,6 +41,5 @@ dataRouter.get('/process-prices', async (req, res, next) => {
     next(error);
   }
 });
-
 
 module.exports = dataRouter;
