19ec99e0da2d678940364bbfeac501e7d697a273
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-25 14:32:43 -0400

    logs

--- src/python/process_data.py
+++ src/python/process_data.py
@@ -5,7 +5,6 @@ def process_data(data):
     print(len(data))
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-        process_data(data)
+    data_str = sys.argv[1]
+    data = json.loads(data_str)
+    process_data(data)
