7f051132d066ab3c68b5add7763236d23d0d29fe
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-26 11:29:01 -0400

    fixing pathing

--- src/data/data-service.js
+++ src/data/data-service.js
@@ -1,5 +1,6 @@
 // data-service.js
 const exec = require('child_process').exec;
+const path = require('path');
 
 const DataService = {
   async getData(db) {
@@ -14,13 +15,14 @@ const DataService = {
 
   processData() {
     return new Promise((resolve, reject) => {
-      exec('python3 process_data.py', (error, stdout, stderr) => {
+      const scriptPath = path.join(__dirname, '../python/process_data.py');
+      console.log('scriptPath:', scriptPath);
+      exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
         if (error) {
           console.error('Error in processData:', error);
           console.error('Stderr:', stderr);
           reject(error);
         } else {
-          console.log('Python script output:', stdout); // Log the output from the Python script
           try {
             resolve(JSON.parse(stdout));
           } catch (parseError) {
