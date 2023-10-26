0741fa93f6833812cc09db8ab361374ed19e7737
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-26 15:37:59 -0400

    mf better work

--- src/data/data-router.js
+++ src/data/data-router.js
@@ -6,24 +6,22 @@ const DataService = require('./data-service');
 router.get('/data', async (req, res, next) => {
   const db = req.app.get('db');
   try {
-    console.log('Entering /data route handler');
     const data = await DataService.getData(db);
-    console.log('Data retrieved:', data);
     res.json(data);
   } catch (error) {
-    console.error('Error in /data route handler:', error);
+    console.error('Error object in /data route handler:', error);
+    console.log('Response object in /data route handler:', res);
     next(error);
   }
 });
 
 router.get('/process', async (req, res, next) => {
   try {
-    console.log('Entering /process route handler');
     const result = await DataService.processData();
-    console.log('Process result:', result);
     res.json({ count: result });
   } catch (error) {
-    console.error('Error in /process route handler:', error);
+    console.error('Error object in /process route handler:', error);
+    console.log('Response object in /process route handler:', res);
     next(error);
   }
 });
