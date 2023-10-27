41bdca84fc97a7354a209a79fa89379a9c59c32e
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-27 19:25:10 -0400

    added error handling

--- src/sentiment/sentiment-service.js
+++ src/sentiment/sentiment-service.js
@@ -199,7 +199,7 @@ const SentimentService = {
         - If the new term does not match any term in the master list but is definitely relevant, assign a value of 0.
         - If the new term is irrelevant for financial analysis or is way too specific to be useful, assign a value of -1.
         ${content}
-        Please do not truncate your response. Please provide the results in the following JSON format with no unnecessary characters:
+        Please do not truncate your response. Generate the entire list. Please provide the results in the following JSON format with no unnecessary characters:
         [{"term": "exampleTerm1", "value": 1}, {"term": "exampleTerm2", "value": 0}]
          `;
         break;
