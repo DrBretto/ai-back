23726d068a7dc6426664c25a4affd9c8d0ba8bed
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-27 19:09:58 -0400

    added error handling

--- src/sentiment/sentiment-service.js
+++ src/sentiment/sentiment-service.js
@@ -201,7 +201,7 @@ const SentimentService = {
         ${content}
         Please provide the results in the following JSON format:
         [{"term": "exampleTerm1", "value": 1}, {"term": "exampleTerm2", "value": 0}]
-        Please do not truncate your response.
+        Please do not truncate your response. Please generate the entire json code so I can parse it properly
         `;
         break;
       default:
