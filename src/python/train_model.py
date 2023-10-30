38194e2ab8b65b312104e736be695e4f1e107b65
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-10-30 15:42:23 -0400

    redid python script, added pandas

--- src/python/train_model.py
+++ src/python/train_model.py
@@ -1,32 +1,46 @@
 import sys
+import pandas as pd
 import json
 
-def inspect_data(data):
+def inspect_data(historical_path, realtime_path, sentiment_path):
+    # Reading data from the CSV files
+    historical_data = pd.read_csv(historical_path)
+    realtime_data = pd.read_csv(realtime_path)
+    sentiment_data = pd.read_csv(sentiment_path)
+
+    # Filtering data by stock_id
+    nugt_historical = historical_data[historical_data['stock_id'] == 1]
+    jdst_historical = historical_data[historical_data['stock_id'] == 2]
+
+    nugt_realtime = realtime_data[realtime_data['stock_id'] == 1]
+    jdst_realtime = realtime_data[realtime_data['stock_id'] == 2]
+
+    nugt_sentiment = sentiment_data[sentiment_data['subject_id'] == 2]
+    jdst_sentiment = sentiment_data[sentiment_data['subject_id'] == 1]
+
     # Extracting the first line of each data section
     first_line = {
         'NUGT': {
-            'historical': data['NUGT']['historical'][0] if data['NUGT']['historical'] else None,
-            'realtime': data['NUGT']['realtime'][0] if data['NUGT']['realtime'] else None,
-            'sentiment': data['NUGT']['sentiment'][0] if data['NUGT']['sentiment'] else None
+            'historical': nugt_historical.iloc[0].to_dict() if not nugt_historical.empty else None,
+            'realtime': nugt_realtime.iloc[0].to_dict() if not nugt_realtime.empty else None,
+            'sentiment': nugt_sentiment.iloc[0].to_dict() if not nugt_sentiment.empty else None
         },
         'JDST': {
-            'historical': data['JDST']['historical'][0] if data['JDST']['historical'] else None,
-            'realtime': data['JDST']['realtime'][0] if data['JDST']['realtime'] else None,
-            'sentiment': data['JDST']['sentiment'][0] if data['JDST']['sentiment'] else None
+            'historical': jdst_historical.iloc[0].to_dict() if not jdst_historical.empty else None,
+            'realtime': jdst_realtime.iloc[0].to_dict() if not jdst_realtime.empty else None,
+            'sentiment': jdst_sentiment.iloc[0].to_dict() if not jdst_sentiment.empty else None
         }
     }
     return first_line
 
 if __name__ == '__main__':
-    data_path = sys.argv[1]
-    result_path = sys.argv[2]
-
-    # Reading data from the file
-    with open(data_path, 'r') as file:
-        data = json.load(file)
+    historical_path = sys.argv[1]
+    realtime_path = sys.argv[2]
+    sentiment_path = sys.argv[3]
+    result_path = sys.argv[4]
 
     # Inspecting the data
-    result = inspect_data(data)
+    result = inspect_data(historical_path, realtime_path, sentiment_path)
 
     # Writing the result to a new file
     with open(result_path, 'w') as file:
