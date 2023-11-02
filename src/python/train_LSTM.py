4c4d6d4cf63508cd1ca7abc1bae109d5729a3670
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-11-02 18:59:50 -0400

    cleaned up data prepocessing entirely

--- src/python/train_LSTM.py
+++ src/python/train_LSTM.py
@@ -1,36 +1,18 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 import psycopg2
 import pandas as pd
 import numpy as np
 import os
 from torch.utils.data import TensorDataset, DataLoader
-import json
 import gc
 import resource
-import time
 import sys
-
-
-def log_memory_usage():
-    memory_usage_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    with open('memory_log.txt', 'a') as f:  # 'a' mode for append to the log file
-        print(f"Memory Usage: {memory_usage_kb / 1024} MB", file=f)
-
 
 
 # Define the window and intervals outside of the function
 lagwindow = 30
 intervals = [1, 15, 60, 1440]
-
-def normalize_data(df):
-    # Perform Min-Max normalization manually using pandas
-    for column in ['closing_price', 'high_price', 'low_price', 'volume']:
-        min_column = df[column].min()
-        max_column = df[column].max()
-        df[column] = (df[column] - min_column) / (max_column - min_column)
-    return df
 
 class SimpleLSTM(nn.Module):
     def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
@@ -50,6 +32,11 @@ class SimpleLSTM(nn.Module):
         return predictions[-1]
 
 
+def log_memory_usage():
+    memory_usage_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    with open('memory_log.txt', 'a') as f:  # 'a' mode for append to the log file
+        print(f"Memory Usage: {memory_usage_kb / 1024} MB", file=f)
+
 def create_lagged_features(stock_data):
     # 'intervals' could be a list of intervals like [1, 15, 60, 1440] representing minutes
     # 'window' could be a fixed size or you can adjust as needed, e.g., 30
@@ -61,6 +48,15 @@ def create_lagged_features(stock_data):
                 lagged_data[lagged_column_name] = stock_data[feature].shift(lag * interval)
     return lagged_data
 
+def calculate_global_min_max(historical_data_jdst, historical_data_nugt):
+    # Combine both stock data sets to calculate global min and max
+    combined_stock_data = pd.concat([historical_data_jdst, historical_data_nugt])
+    
+    # Calculate global min and max for each column
+    global_min = combined_stock_data[['closing_price', 'high_price', 'low_price', 'volume']].min()
+    global_max = combined_stock_data[['closing_price', 'high_price', 'low_price', 'volume']].max()
+    
+    return global_min, global_max
 
 def combine_and_average_sentiments(sentiment_data):
     # Average the scores and combine the tokenized sentiments
@@ -74,16 +70,29 @@ def combine_and_average_sentiments(sentiment_data):
     return averaged_data
 
 def process_sentiment_data(sentiment_data):
-    # Separate by subject_id
-    sentiment_gold = sentiment_data[sentiment_data['subject_id'] == 1]
-    sentiment_usd = sentiment_data[sentiment_data['subject_id'] == 2]
+    # First, ensure no duplicate tokens in the 'token_values' column
+    sentiment_data['token_values'] = sentiment_data['token_values'].apply(lambda x: list(set(x)))
+
+    # Now, for each 'subject_id' and 'date_published' pair, we'll combine token values and average scores if there are duplicates
+    processed_sentiment = sentiment_data.groupby(['date_published', 'subject_id'], as_index=False).agg({
+        'high_score': 'mean',
+        'low_score': 'mean',
+        'average_score': 'mean',
+        'token_values': lambda x: list(set().union(*x))
+    })
+
+    return processed_sentiment
+
+def normalize_data_global_and_impute(stock_data, global_min, global_max):
+    # Assuming that 'stock_data' includes data for both stocks and is ready for global normalization
+    # Perform Min-Max normalization across the entire dataset using the provided global min and max
+    for column in ['closing_price', 'high_price', 'low_price', 'volume']:
+        stock_data[column] = (stock_data[column] - global_min[column]) / (global_max[column] - global_min[column])
     
-    # Combine and average sentiments
-    sentiment_gold = combine_and_average_sentiments(sentiment_gold)
-    sentiment_usd = combine_and_average_sentiments(sentiment_usd)
-
-    return sentiment_gold, sentiment_usd
-
+    # Forward fill to impute missing values for each stock
+    imputed_data = stock_data.ffill().bfill()
+    
+    return imputed_data
 
 def process_in_batches(df, batch_size):
     for start in range(0, len(df), batch_size):           
@@ -91,12 +100,9 @@ def process_in_batches(df, batch_size):
         sys.stdout.flush()
         end = min(start + batch_size, len(df))
         batch = df[start:end]
-        # Normalize batch
-        batch_normalized = normalize_data(batch)
-        # Create lagged features for batch
-        batch_with_lagged_features = create_lagged_features(batch_normalized, [1, 15, 60, 1440])
+        # Create lagged features for batch without re-normalizing
+        batch_with_lagged_features = create_lagged_features(batch, [1, 15, 60, 1440])
         yield batch_with_lagged_features
-
 
 def get_data_from_db(chunksize=10000):
     print(f"=========before loading data from db===========")
@@ -148,24 +154,14 @@ def get_data_from_db(chunksize=10000):
 
     return historical_data_jdst, historical_data_nugt, sentiment_gold, sentiment_usd
 
-
-def integrate_sentiment(stock_data, sentiment_gold, sentiment_usd):
-    # Ensure that 'date_time' in stock_data is at the same granularity as 'date_published' in sentiment data
-    
-    # Forward-fill the sentiment data so that each stock timestamp has the latest available sentiment scores
-    stock_data_with_gold = pd.merge_asof(stock_data.sort_values('date_time'), 
-                                         sentiment_gold.sort_values('date_published'),
-                                         left_on='date_time', right_on='date_published', 
-                                         by='subject_id', direction='forward', suffixes=('', '_gold'))
-    
-    stock_data_with_gold_usd = pd.merge_asof(stock_data_with_gold.sort_values('date_time'), 
-                                             sentiment_usd.sort_values('date_published'),
-                                             left_on='date_time', right_on='date_published', 
-                                             by='subject_id', direction='forward', suffixes=('', '_usd'))
-    
-    # Now stock_data_with_gold_usd contains both gold and USD sentiment scores for each timestamp
-    return stock_data_with_gold_usd
-
+def integrate_sentiment(stock_data, sentiment_data):
+    # Instead of duplicating sentiment data, join it with the stock data
+    # The join will be performed such that each row in stock_data will be matched with the corresponding sentiment data based on the date
+    stock_data_with_sentiment = pd.merge_asof(stock_data.sort_values('date_time'), 
+                                              sentiment_data.sort_values('date_published'),
+                                              left_on='date_time', right_on='date_published', 
+                                              direction='forward')
+    return stock_data_with_sentiment
 
 def prepare_dataloaders(stock_data_with_sentiment, batch_size):
     dataloaders = []
@@ -180,35 +176,32 @@ def prepare_dataloaders(stock_data_with_sentiment, batch_size):
         dataloaders.append(dataloader)
     return dataloaders
 
-
 def process_data(batch_size):
-    # Fetch the split data from the DB
+    # Fetch the data from the DB
     historical_data_jdst, historical_data_nugt, sentiment_gold, sentiment_usd = get_data_from_db()
 
-    # Integrate sentiment scores with stock data
-    historical_data_jdst_with_sentiment = integrate_sentiment(historical_data_jdst, sentiment_gold, sentiment_usd)
-    historical_data_nugt_with_sentiment = integrate_sentiment(historical_data_nugt, sentiment_gold, sentiment_usd)
+    # Combine and normalize the datasets without duplicating sentiment data
+    combined_data = pd.concat([historical_data_jdst, historical_data_nugt])
 
-    # Combine the datasets
-    combined_data = pd.concat([historical_data_jdst_with_sentiment, historical_data_nugt_with_sentiment])
-    combined_data.sort_values(by='date_time', inplace=True)  # Ensure the data is sorted by date_time
+    # Obtain global min and max for normalization
+    global_min, global_max = calculate_global_min_max(historical_data_jdst, historical_data_nugt)
 
-    # Normalize the combined data
-    combined_data_normalized = combined_data.apply(normalize_data)
+    # Normalize and impute the combined data
+    combined_data_normalized_imputed = normalize_data_global_and_impute(combined_data, global_min, global_max)
 
-    # Process the normalized combined data in batches
-    dataloaders = prepare_dataloaders(combined_data_normalized, batch_size)
+    # Integrate sentiment scores with each stock's normalized and imputed data separately
+    combined_data_jdst_with_sentiment = integrate_sentiment(combined_data_normalized_imputed[combined_data_normalized_imputed['stock_id'] == 1], sentiment_gold)
+    combined_data_nugt_with_sentiment = integrate_sentiment(combined_data_normalized_imputed[combined_data_normalized_imputed['stock_id'] == 2], sentiment_usd)
 
-    # At this point, you would typically have a loop to iterate over dataloaders
-    # and perform operations like training your model, etc.
-    # However, since we are focusing on the data processing part here, we can
-    # skip this step or include it based on your specific needs.
+    # Re-combine the data with sentiment
+    final_combined_data = pd.concat([combined_data_jdst_with_sentiment, combined_data_nugt_with_sentiment])
 
-    latest_data_snapshot = combined_data_normalized.tail(1)
+    # Process the final combined data in batches
+    dataloaders = prepare_dataloaders(final_combined_data, batch_size)
+
+    # The rest of the code remains the same...
+    latest_data_snapshot = final_combined_data.tail(1)
     return latest_data_snapshot  # Return the dataloaders as well if they will be used later.
-
-
-
 
 
 if __name__ == '__main__':
@@ -228,32 +221,4 @@ if __name__ == '__main__':
 
     # Print the JSON snapshot
     print(json_snapshot)
-
-
-
-# if __name__ == '__main__':
-#     dataloader = process_data()
-#     model = SimpleLSTM()
-#     loss_function = nn.MSELoss()
-#     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-#     epochs = 150
-#     for i in range(epochs):
-#         for seq, labels in dataloader:
-#             optimizer.zero_grad()
-#             model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
-#                             torch.zeros(1, 1, model.hidden_layer_size))
-
-#             y_pred = model(seq)
-
-#             single_loss = loss_function(y_pred, labels)
-#             single_loss.backward()
-#             optimizer.step()
-
-#             if i%25 == 1:
-#                 print(f'Epoch {i} Iteration loss: {single_loss.item()}')
-
-#     print(json.dumps({'message': 'Data handled successfully'}))
-
-
 
