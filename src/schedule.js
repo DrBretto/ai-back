9c0d8ff007e22ab4d2bf6305e4b4da5dd48f58d3
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-11-27 13:59:40 -0500

    updating price cron to run slower to avoid conflicts

--- src/schedule.js
+++ src/schedule.js
@@ -11,67 +11,61 @@ module.exports = (app) => {
     await StocksService.fetchHistoricalData(db, 'NUGT');
   });
 
-  // cron.schedule('*/1 * * * *', async () => {
-  //   //realtime prices every minute - NYI
-  //   const db = app.get('db');
-  //   await StocksService.fetchTodaysData(db, 'JDST');
-  //   await StocksService.fetchTodaysData(db, 'NUGT');
-  // });
 
-  // cron.schedule('*/1 * * * *', () => {
-  //   //TradingView Scrape and analyze
-  //   const db = app.get('db');
-  //   console.log(
-  //     'Running sentiment analysis for gold and dollar at:',
-  //     new Date()
-  //   );
+  cron.schedule('*/1 * * * *', () => {
+    //TradingView Scrape and analyze
+    const db = app.get('db');
+    console.log(
+      'Running sentiment analysis for gold and dollar at:',
+      new Date()
+    );
 
-  //   SentimentService.performSentimentAnalysis(db, 'gold', 'tradingview')
-  //     .then(() => {
-  //       console.log('Successfully analyzed sentiment for gold');
-  //     })
-  //     .catch((err) => {
-  //       console.error('Error analyzing sentiment for gold:', err.code);
-  //     });
+    SentimentService.performSentimentAnalysis(db, 'gold', 'tradingview')
+      .then(() => {
+        console.log('Successfully analyzed sentiment for gold');
+      })
+      .catch((err) => {
+        console.error('Error analyzing sentiment for gold:', err.code);
+      });
 
-  //   SentimentService.performSentimentAnalysis(db, 'dollar', 'tradingview')
-  //     .then(() => {
-  //       console.log('Successfully analyzed sentiment for dollar:');
-  //     })
-  //     .catch((err) => {
-  //       console.error('Error analyzing sentiment for dollar:', err.code);
-  //     });
-  // });
+    SentimentService.performSentimentAnalysis(db, 'dollar', 'tradingview')
+      .then(() => {
+        console.log('Successfully analyzed sentiment for dollar:');
+      })
+      .catch((err) => {
+        console.error('Error analyzing sentiment for dollar:', err.code);
+      });
+  });
 
-  // cron.schedule('*/1 * * * *', async () => {
-  //   //backlog Summary Tokenizer
-  //   const db = app.get('db');
-  //   for (let i = 0; i < 5; i++) {
-  //     // Loop to process up to 5 entries
-  //     try {
-  //       const entry = await db('sentiment_analysis')
-  //         .whereNull('token_values')
-  //         .orderBy('id', 'asc') // Ensure entries are processed in order of their id
-  //         .first();
+  cron.schedule('*/1 * * * *', async () => {
+    //backlog Summary Tokenizer
+    const db = app.get('db');
+    for (let i = 0; i < 5; i++) {
+      // Loop to process up to 5 entries
+      try {
+        const entry = await db('sentiment_analysis')
+          .whereNull('token_values')
+          .orderBy('id', 'asc') // Ensure entries are processed in order of their id
+          .first();
 
-  //       if (entry) {
-  //         // If an unprocessed entry is found, trigger your sentiment analysis functions
-  //         const sentimentAnalysisId = entry.id;
-  //         await SentimentService.performTermComparison(db, sentimentAnalysisId);
-  //         console.log(
-  //           'Successfully processed entry with id:',
-  //           sentimentAnalysisId
-  //         );
-  //       } else {
-  //         console.log('No unprocessed entries found.');
-  //         break; // Exit loop if no unprocessed entry is found
-  //       }
-  //     } catch (error) {
-  //       console.error('Error processing unprocessed entry:', error.code);
-  //       break; // Exit loop in case of an error
-  //     }
-  //   }
-  // });
+        if (entry) {
+          // If an unprocessed entry is found, trigger your sentiment analysis functions
+          const sentimentAnalysisId = entry.id;
+          await SentimentService.performTermComparison(db, sentimentAnalysisId);
+          console.log(
+            'Successfully processed entry with id:',
+            sentimentAnalysisId
+          );
+        } else {
+          console.log('No unprocessed entries found.');
+          break; // Exit loop if no unprocessed entry is found
+        }
+      } catch (error) {
+        console.error('Error processing unprocessed entry:', error.code);
+        break; // Exit loop in case of an error
+      }
+    }
+  });
 
   // //Backlog Historical Schedulers////////////////////////////////////////
 
